c4faed54a844b3d08cf435c29909e589326d986d
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-02-02 11:18:22 -0500

    worked on user input promps in index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+    default: "Camila",
+},
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+    default: "moreiras.camila@gmail.com",
+},
+{
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+    default: "Readme Generator",
+},
+{
+    type: "input",
+    message: "Please describe your project.",
+    name: "description",
+    default: "This project generates a README.md file using the inquirer library package and node.js.",
+},
+{
+    type: "input",
+    message: "Please describe how to install your project.",
+    name: "installation",
+    default: "npm install inquirer@8.2.4",
+},
+{
+    type: "input",
+    message: "Please describe what the application is used for.",
+    name: "usage",
+    default: "Downloading inquirer will allow you to run the npm language in node.js, so that you can build out your application.",
+},
+{
+    type: "input",
+    message: "Please describe how to report bugs.",
+    name: "bugs",
+    //!default: "this is how to report bugs",
+},
+{
+    //!type: "list",
+    message: "What license would you like to use?",
+    name: "license",
+    //!default: "MIT",
+},
+{
+    type: "input",
+    message: "Please describe how to contribute.",
+    name: "contribution",
+    //!default: "This is how to contribute",
+}
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        fs.writeToFile("README.md", generateMarkdown(data), (err) => 
+        err ? console.error(err) : console.log("Success!"));
+    })
+    .then((response.name) => console.log(response)); 
+    //!do I have to do this for all questions?
+}
+
+//!Is this function needed? It wasn't in TODO but seems like it should be here.
+function inquirerResponse(response) {
+
+}
+
 
 // Function call to initialize app
-init();
+init()
+;
