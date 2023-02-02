3266e3e9c1c7c06929232c399ee1373801dc7db8
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-02-02 12:54:01 -0500

    added functionality to generateMarkdown and index files

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
-# Professional README Generator Starter Code
+# Readme Generator
+![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+## Description
+
+This project generates a README.md file using the inquirer library package and node.js.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+npm install
+```
+
+
+## Usage
+
+Downloading inquirer will allow you to run the npm language in node.js, so that you can build out your application.
+
+## License
+    
+This project is licensed under the MIT license.
+
+## Contributing
+
+undefined
+
+## Tests
+
+To run tests, run the following command:
+
+```
+undefined
+```
+
+## Questions
+
+For questions, feel free to reach out via email at moreiras.camila@gmail.com. You can find more of my work at [cmv303](https://github.com/cmv303/)
--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
+// TODO: make all table of contents sections linkable to proper section in readme
 const questions = [{
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "username",
-    default: "Camila",
-},
-{
-    type: "input",
-    message: "What is your email address?",
-    name: "email",
-    default: "moreiras.camila@gmail.com",
-},
-{
     type: "input",
     message: "What is the title of your project?",
     name: "title",
@@ -31,7 +22,7 @@ const questions = [{
     type: "input",
     message: "Please describe how to install your project.",
     name: "installation",
-    default: "npm install inquirer@8.2.4",
+    default: "npm install",
 },
 {
     type: "input",
@@ -41,15 +32,15 @@ const questions = [{
 },
 {
     type: "input",
-    message: "Please describe how to report bugs.",
-    name: "bugs",
+    message: "Please describe how to report issues.",
+    name: "issues",
     default: "In the repository, navigate to the issues section and click on the link to report a bug/create an issue.",
 },
 {
     type: "list",
     message: "What license would you like to use?",
     name: "license",
-    choices: ["MIT", "Apache 2.0", "GPL 3.0",],
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "none",],
     default: "MIT",
 },
 {
@@ -57,27 +48,42 @@ const questions = [{
     message: "Please describe how to contribute.",
     name: "contribution",
     default: "If you would like to contribute to this project, please fork the repo, create a new feature branch. Once you have finished, you can create a pull request.",
+},
+{
+    type: "input",
+    message: "What command should be used to run a test?",
+    name: "tests",
+    default: "npm run test",
+},
+{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+    default: "cmv303"
+},
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+    default: "moreiras.camila@gmail.com"
 }
-
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName), data)
+}
 
 // TODO: Create a function to initialize app
 function init() {
+    //first ask the user questions
     inquirer.prompt(questions)
-    .then((data) => {
-        fs.writeToFile("README.md", generateMarkdown(data), (err) => 
-        err ? console.error(err) : console.log("Success!"));
+    //then write a promise to get the answers
+    .then((inquirerResponse) => {
+        console.log("Generating your readme");
+        //exectuting the promise
+        writeToFile ("README.md", generateMarkdown({...inquirerResponse}))
     })
-    .then((response.name) => console.log(response));
-    //!do I have to do this for all questions?
-}
-
-//!Is this function needed? It wasn't in TODO but seems like it should be here.
-function inquirerResponse(response) {
-
 }
 
 
